7e716621db9c06cba463545276c24ab3df88cb50
Darío Javier Cravero <dario@uxtemple.com>
2018-05-30 00:56:59 +0100

    feat: animate cards

---
+++ web/src/fix-animated.js
@@ -0,0 +1,5 @@
+import Animated from 'animated/lib/targets/react-dom';
+
+Animated.a = Animated.createAnimatedComponent('a')
+Animated.button = Animated.createAnimatedComponent('button')
+Animated.span = Animated.createAnimatedComponent('span')
--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,4 @@
+import './fix-animated.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
